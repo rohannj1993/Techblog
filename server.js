a8d9196e99f11dae3c220bb4ea026efc9376c3e1
rohannj1993 <rohannj1993@gmail.com>
2021-08-12 12:04:47 -0400

    updated stylesheet

---
+++ server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
