572e9a597f57899d1db48736a81a31f0096092e0
rohannj1993 <rohannj1993@gmail.com>
2021-08-15 20:10:39 -0400

    added login.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const path = require('path');
 const routes = require('./controllers/');
-app.use(express.static(path.join(__dirname, 'public')));
-
-
-
+const express = require("express")
+const session = require("express-session")
+const sequelizeStore = require ("connect-session-sequelize")
+const PORT = 3000
+const app = express()
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
