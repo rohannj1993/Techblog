e3b93edd1a6ca5a84eab07ea181e10402d7167d4
rohannj1993 <rohannj1993@gmail.com>
2021-08-16 20:22:29 -0400

    updated nore handlebars

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const routes = require('./controllers/');
+const routes = require('./controllers/dashboard-routes');
 const express = require("express")
 const session = require("express-session")
 const sequelize =""
