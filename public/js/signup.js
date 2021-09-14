d4bbb03bf010cfc4260763273fc3b7c3dc0d9e6c
rohannj1993 <rohannj1993@gmail.com>
2021-09-14 08:45:34 -0400

    updated signup form with email

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,19 +4,19 @@ function signupFormHandler(event) {
   
     const username = document.querySelector('#username-input-signup').value.trim();
     
-    // const email = document.querySelector('#email-signup').value.trim();
+   const email = document.querySelector('#email-signup').value.trim();
     
     const password = document.querySelector('#password-input-signup').value.trim();
-    console.log(username),
-    console.log(password)
+    // console.log(username),
+    // console.log(password)
     
   
-    if (username  && password) {
+    if (username  && password && email) {
       const response = fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
-          // email,
+           email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
@@ -31,4 +31,4 @@ function signupFormHandler(event) {
     }
     }
   
-document.querySelector('#signup-forum').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
